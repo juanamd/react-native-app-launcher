5d2b264016aa45106c94341762bedf1f51d54eb2
juanamd <juanstory@gmail.com>
2017-07-26 16:50:31 -0300

    Fix missing import

--- android/src/main/java/io/cmichel/appLauncher/AlarmReceiver.java
+++ android/src/main/java/io/cmichel/appLauncher/AlarmReceiver.java
@@ -3,6 +3,7 @@ package io.cmichel.appLauncher;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver {
